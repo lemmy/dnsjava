d947787c8fe63093fbfbdba153dc74016fad4105
(no author) <(no author)@01ac7c06-4ada-475d-a7e9-4678e8e00964>
2004-02-04 18:25:26 +0000

    This commit was manufactured by cvs2svn to create tag 'dnsjava-1-5-2-pre'.
    git-svn-id: https://dnsjava.svn.sourceforge.net/svnroot/dnsjava@1192 01ac7c06-4ada-475d-a7e9-4678e8e00964

---
+++ tags/dnsjava-1-5-2-pre/org/xbill/DNS/KEYRecord.java
@@ -0,0 +1,97 @@
+// Copyright (c) 1999-2004 Brian Wellington (bwelling@xbill.org)
+
+package org.xbill.DNS;
+
+import java.io.*;
+import org.xbill.DNS.utils.*;
+
+/**
+ * Key - contains a cryptographic public key.  The data can be converted
+ * to objects implementing java.security.interfaces.PublicKey
+ * @see DNSSEC
+ *
+ * @author Brian Wellington
+ */
+
+public class KEYRecord extends KEYBase {
+
+private static KEYRecord member = new KEYRecord();
+
+/* flags */
+/** This key cannot be used for confidentiality (encryption) */
+public static final int FLAG_NOCONF = 0x8000;
+
+/** This key cannot be used for authentication */
+public static final int FLAG_NOAUTH = 0x4000;
+
+/** This key cannot be used for authentication or confidentiality */
+public static final int FLAG_NOKEY = 0xC000;
+
+/** A zone key */
+public static final int OWNER_ZONE = 0x0100;
+
+/** A host/end entity key */
+public static final int OWNER_HOST = 0x0200;
+
+/** A user key */
+public static final int OWNER_USER = 0x0000;
+
+/* protocols */
+/** Key was created for use with transaction level security */
+public static final int PROTOCOL_TLS = 1;
+
+/** Key was created for use with email */
+public static final int PROTOCOL_EMAIL = 2;
+
+/** Key was created for use with DNSSEC */
+public static final int PROTOCOL_DNSSEC = 3;
+
+/** Key was created for use with IPSEC */
+public static final int PROTOCOL_IPSEC = 4;
+
+/** Key was created for use with any protocol */
+public static final int PROTOCOL_ANY = 255;
+
+private
+KEYRecord() {}
+
+private
+KEYRecord(Name name, int dclass, long ttl) {
+	super(name, Type.KEY, dclass, ttl);
+}
+
+static KEYRecord
+getMember() {
+	return member;
+}
+
+/**
+ * Creates a KEY Record from the given data
+ * @param flags Flags describing the key's properties
+ * @param proto The protocol that the key was created for
+ * @param alg The key's algorithm
+ * @param key Binary data representing the key
+ */
+public
+KEYRecord(Name name, int dclass, long ttl, int flags, int proto, int alg,
+	  byte [] key)
+{
+	super(name, Type.KEY, dclass, ttl, flags, proto, alg, key);
+}
+
+Record
+rrFromWire(Name name, int type, int dclass, long ttl, int length,
+	   DataByteInputStream in)
+throws IOException
+{
+	return rrFromWire(new KEYRecord(name, dclass, ttl), length, in);
+}
+
+Record
+rdataFromString(Name name, int dclass, long ttl, Tokenizer st, Name origin)
+throws IOException
+{
+	return rdataFromString(new KEYRecord(name, dclass, ttl), st, origin);
+}
+
+}
